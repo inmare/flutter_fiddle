380179ccc19a6e1a9bca2d1f69d2c746e4ad44a2
inmare <inmare2000@proton.me>
2026-02-05 03:41:29 +0900

    demucs 추론 함수 추가

--- native/demucs_bridge/demucs_bridge.cpp
+++ native/demucs_bridge/demucs_bridge.cpp
@@ -1,337 +1,193 @@
-// 여기에서 만들어야 할 거
-// onnx모델 로딩 함수
-// onnx모델 처리 함수
-// float32 array를 eigen으로 변환한 다음, 모델로 넘겨주는 함수
-
-#include "demucs.hpp"
-// #include <algorithm>
-// #include <cmath>
-// #include <cstdlib>
-// #include <cstring>
-// #include <filesystem>
-#include <fstream>
-#include <iostream>
-// #include <libnyquist/Common.h>
-// #include <libnyquist/Decoders.h>
-// #include <libnyquist/Encoders.h>
-// #include <map>
-// #include <numeric>
-// #include <ranges>
-// #include <sstream>
-#include <stddef.h>
-// #include <tuple>
-#include <vector>
 #include "demucs_bridge.h"
-
-// using namespace nqr;
+#include "demucs.hpp"
+#include "tensor.hpp"
+#include <chrono>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <stddef.h>
+#include <vector>
 
 static demucsonnx::demucs_model model;
 static bool model_loaded;
 
-static bool demucs_load_model(
-    const std::string &htdemucs_model_path,
-    int intraNumThreads,
-    int interNumThreads)
-{
-    // create Ort::SessionOptions
-    Ort::SessionOptions session_options;
+extern "C" {
 
-    // max out threads and increase performance to the max on my beefy
-    // desktop CPU
-    session_options.SetExecutionMode(ExecutionMode::ORT_PARALLEL);
-    session_options.SetIntraOpNumThreads(intraNumThreads);
-    session_options.SetInterOpNumThreads(interNumThreads);
+FFI_EXPORT bool
+demucs_load_model(const char *htdemucs_model_path, int intraNumThreads,
+                  int interNumThreads) { // create Ort::SessionOptions
+  Ort::SessionOptions session_options;
 
-    // General optimizations
-    session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
+  // max out threads and increase performance to the max on my beefy
+  // desktop CPU
+  session_options.SetExecutionMode(ExecutionMode::ORT_PARALLEL);
+  session_options.SetIntraOpNumThreads(intraNumThreads);
+  session_options.SetInterOpNumThreads(interNumThreads);
 
-    // struct demucsonnx::demucs_model model;
+  // General optimizations
+  // ORT_ENABLE_ALL: 모든 최적화 수행
+  // ORT_ENABLE_BASIC: 기본 최적화 수행
+  // ORT_DISABLE_ALL: 최적화 수행 안함
+  session_options.SetGraphOptimizationLevel(
+      GraphOptimizationLevel::ORT_ENABLE_ALL);
 
-    std::ifstream file(htdemucs_model_path, std::ios::binary | std::ios::ate);
-    if (!file)
-    {
-        return false;
-        // throw std::runtime_error("Failed to open model file: " + htdemucs_model_path);
-    }
+  // struct demucsonnx::demucs_model model;
 
-    std::streamsize size = file.tellg();
-    file.seekg(0, std::ios::beg);
+  std::ifstream file(htdemucs_model_path, std::ios::binary | std::ios::ate);
+  if (!file) {
+    model_loaded = false;
+    std::cerr << "Failed to open model file: " << htdemucs_model_path
+              << std::endl;
+    return model_loaded;
+    // throw std::runtime_error("Failed to open model file: " +
+    // htdemucs_model_path);
+  }
 
-    std::vector<char> file_data(size);
-    if (!file.read(file_data.data(), size))
-    {
-        // throw std::runtime_error("Failed to read model file.");
-        return false;
-    }
+  std::streamsize size = file.tellg();
+  file.seekg(0, std::ios::beg);
 
-    bool success = demucsonnx::load_model(file_data, model, session_options);
-    if (!success)
-    {
-        // throw std::runtime_error("Failed to load model.");
-        return false;
-    }
+  std::vector<char> file_data(size);
+  if (!file.read(file_data.data(), size)) {
+    // throw std::runtime_error("Failed to read model file.");
+    model_loaded = false;
+    std::cerr << "Failed to read model file: " << htdemucs_model_path
+              << std::endl;
+    return model_loaded;
+  }
 
-    return true;
+  // std::string model_path_string
+
+  bool success = demucsonnx::load_model(file_data, model, session_options);
+  if (!success) {
+    // throw std::runtime_error("Failed to load model.");
+    model_loaded = false;
+    std::cerr << "Failed to load model: " << htdemucs_model_path << std::endl;
+    return false;
+  }
+
+  model_loaded = true;
+  return model_loaded;
 }
 
-// Overload for file path, calling one of the other overloads as needed
-// static demucsonnx::demucs_model load_model(
-//     const std::string &htdemucs_model_path,
-//     Ort::SessionOptions &session_options)
-// {
-//     struct demucsonnx::demucs_model model;
+FFI_EXPORT void demucs_split_audio(const float *audioData,
+                                   const int numSamplesPerChannel,
+                                   const int numChannels, const int sampleRate,
+                                   float *outDrums, float *outBass,
+                                   float *outOther, float *outVocals) {
+  if (!model_loaded) {
+    std::cerr << "Model not loaded." << std::endl;
+    return;
+  }
 
-//     std::ifstream file(htdemucs_model_path, std::ios::binary | std::ios::ate);
-//     if (!file)
-//     {
-//         throw std::runtime_error("Failed to open model file: " + htdemucs_model_path);
-//     }
+  // 파일 데이터를 eigen matrix로 변환환
+  Eigen::MatrixXf audio(2, numSamplesPerChannel);
 
-//     std::streamsize size = file.tellg();
-//     file.seekg(0, std::ios::beg);
+  if (numChannels == 1) {
+    // Mono case
+    for (std::size_t i = 0; i < numSamplesPerChannel; ++i) {
+      audio(0, i) = audioData[i]; // left channel
+      audio(1, i) = audioData[i]; // right channel
+    }
+  } else {
+    // Stereo case
+    for (std::size_t i = 0; i < numSamplesPerChannel; ++i) {
+      audio(0, i) = audioData[2 * i];     // left channel
+      audio(1, i) = audioData[2 * i + 1]; // right channel
+    }
+  }
 
-//     std::vector<char> file_data(size);
-//     if (!file.read(file_data.data(), size))
-//     {
-//         throw std::runtime_error("Failed to read model file.");
-//     }
+  // 읽어온 데이터를 기반으로 inference 수행행
+  // Eigen::MatrixXf audio = load_audio_file(wav_file);
+  std::cout << "Running Demucs.onnx inference." << std::endl;
 
-//     bool success = demucsonnx::load_model(file_data, model, session_options);
-//     if (!success)
-//     {
-//         throw std::runtime_error("Failed to load model.");
-//     }
+  // set output precision to 3 decimal places
+  std::cout << std::fixed << std::setprecision(3);
 
-//     return model;
-// }
+  demucsonnx::ProgressCallback progressCallback =
+      [](float progress, const std::string &log_message) {
+        std::cout << "(" << std::setw(3) << std::setfill(' ')
+                  << progress * 100.0f << "%) " << log_message << std::endl;
+      };
 
-// static Eigen::MatrixXf load_audio_file(std::string filename)
-// {
-//     // load a wav file with libnyquist
-//     std::shared_ptr<AudioData> fileData = std::make_shared<AudioData>();
+  // create 4 audio matrix same size, to hold output
+  Eigen::Tensor3dXf out_targets;
+  auto start_time = std::chrono::high_resolution_clock::now();
 
-//     NyquistIO loader;
+  try {
+    Eigen::Tensor3dXf audio_targets =
+        demucsonnx::demucs_inference(model, audio, progressCallback);
 
-//     loader.Load(fileData.get(), filename);
+    if (audio_targets.size() == 0) {
+      std::cerr << "Inference failed! No output." << std::endl;
+      return;
+    }
 
-//     if (fileData->sampleRate != demucsonnx::SUPPORTED_SAMPLE_RATE)
-//     {
-//         std::cerr << "[ERROR] demucs.cpp only supports the following sample "
-//                      "rate (Hz): "
-//                   << demucsonnx::SUPPORTED_SAMPLE_RATE << std::endl;
-//         exit(1);
-//     }
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+        end_time - start_time);
 
-//     std::cout << "Input samples: "
-//               << fileData->samples.size() / fileData->channelCount << std::endl;
-//     std::cout << "Length in seconds: " << fileData->lengthSeconds << std::endl;
-//     std::cout << "Number of channels: " << fileData->channelCount << std::endl;
+    std::cout << "Inference time: " << duration.count() << " ms" << std::endl;
 
-//     if (fileData->channelCount != 2 && fileData->channelCount != 1)
-//     {
-//         std::cerr << "[ERROR] demucs.cpp only supports mono and stereo audio"
-//                   << std::endl;
-//         exit(1);
-//     }
+    std::cout << "Inference successful! Shape: " << audio_targets.dimension(0)
+              << "x" << audio_targets.dimension(1) << "x"
+              << audio_targets.dimension(2) << std::endl;
 
-//     // number of samples per channel
-//     std::size_t N = fileData->samples.size() / fileData->channelCount;
+    out_targets = audio_targets;
+  } catch (const std::exception &e) {
+    std::cerr << "Inference failed with exception: " << e.what() << std::endl;
+    return;
+  } catch (...) {
+    std::cerr << "Inference failed with unknown exception." << std::endl;
+    return;
+  }
 
-//     // create a struct to hold two float vectors for left and right channels
-//     Eigen::MatrixXf ret(2, N);
+  int nb_out_sources = model.nb_sources;
+  int output_samples = out_targets.dimension(2);
+  int loop_samples = (output_samples < numSamplesPerChannel)
+                         ? output_samples
+                         : numSamplesPerChannel;
 
-//     if (fileData->channelCount == 1)
-//     {
-//         // Mono case
-//         for (std::size_t i = 0; i < N; ++i)
-//         {
-//             ret(0, i) = fileData->samples[i]; // left channel
-//             ret(1, i) = fileData->samples[i]; // right channel
-//         }
-//     }
-//     else
-//     {
-//         // Stereo case
-//         for (std::size_t i = 0; i < N; ++i)
-//         {
-//             ret(0, i) = fileData->samples[2 * i];     // left channel
-//             ret(1, i) = fileData->samples[2 * i + 1]; // right channel
-//         }
-//     }
+  for (int target = 0; target < nb_out_sources; ++target) {
+    std::string target_name;
+    float *target_buffer = nullptr;
 
-//     return ret;
-// }
+    switch (target) {
+    case 0:
+      target_name = "drums";
+      target_buffer = outDrums;
+      break;
+    case 1:
+      target_name = "bass";
+      target_buffer = outBass;
+      break;
+    case 2:
+      target_name = "other";
+      target_buffer = outOther;
+      break;
+    case 3:
+      target_name = "vocals";
+      target_buffer = outVocals;
+      break;
+    default:
+      continue;
+    }
 
-// // write a function to write a StereoWaveform to a wav file
-// static void write_audio_file(const Eigen::MatrixXf &waveform,
-//                              std::string filename)
-// {
-//     // create a struct to hold the audio data
-//     std::shared_ptr<AudioData> fileData = std::make_shared<AudioData>();
+    if (target_buffer == nullptr) {
+      continue;
+    }
 
-//     // set the sample rate
-//     fileData->sampleRate = demucsonnx::SUPPORTED_SAMPLE_RATE;
+    Eigen::MatrixXf target_waveform(2, audio.cols());
 
-//     // set the number of channels
-//     fileData->channelCount = 2;
+    // copy the input stereo wav file into all 4 targets
+    for (int i = 0; i < loop_samples; ++i) {
+      target_buffer[2 * i] = out_targets(target, 0, i);
+      target_buffer[2 * i + 1] = out_targets(target, 1, i);
+    }
+  }
 
-//     // set the number of samples
-//     fileData->samples.resize(waveform.cols() * 2);
-
-//     // write the left channel
-//     for (long int i = 0; i < waveform.cols(); ++i)
-//     {
-//         fileData->samples[2 * i] = waveform(0, i);
-//         fileData->samples[2 * i + 1] = waveform(1, i);
-//     }
-
-//     int encoderStatus =
-//         encode_wav_to_disk({fileData->channelCount, PCM_FLT, DITHER_TRIANGLE},
-//                            fileData.get(), filename);
-//     std::cout << "Encoder Status: " << encoderStatus << std::endl;
-// }
-
-// int main(int argc, const char **argv)
-// {
-//     if (argc != 4)
-//     {
-//         std::cerr << "Usage: " << argv[0] << " <model file> <wav file> <out dir>"
-//                   << std::endl;
-//         exit(1);
-//     }
-
-//     std::cout << "demucs.onnx Main driver program" << std::endl;
-//     std::string model_file = argv[1];
-
-//     // load audio passed as argument
-//     std::string wav_file = argv[2];
-
-//     // output dir passed as argument
-//     std::string out_dir = argv[3];
-
-//     // Check if the output directory exists, and create it if not
-//     std::filesystem::path output_dir_path(out_dir);
-//     if (!std::filesystem::exists(output_dir_path))
-//     {
-//         std::cerr << "Directory does not exist: " << out_dir << ". Creating it."
-//                   << std::endl;
-//         if (!std::filesystem::create_directories(output_dir_path))
-//         {
-//             std::cerr << "Error: Unable to create directory: " << out_dir
-//                       << std::endl;
-//             return 1;
-//         }
-//     }
-//     else if (!std::filesystem::is_directory(output_dir_path))
-//     {
-//         std::cerr << "Error: " << out_dir << " exists but is not a directory!"
-//                   << std::endl;
-//         return 1;
-//     }
-
-//     Eigen::MatrixXf audio = load_audio_file(wav_file);
-//     Eigen::Tensor3dXf out_targets;
-
-//     std::cout << "Running Demucs.onnx inference for: " << wav_file << std::endl;
-
-//     // set output precision to 3 decimal places
-//     std::cout << std::fixed << std::setprecision(3);
-
-//     demucsonnx::ProgressCallback progressCallback =
-//         [](float progress, const std::string &log_message)
-//     {
-//         std::cout << "(" << std::setw(3) << std::setfill(' ')
-//                   << progress * 100.0f << "%) " << log_message << std::endl;
-//     };
-
-//     // create Ort::SessionOptions
-//     Ort::SessionOptions session_options;
-
-//     // max out threads and increase performance to the max on my beefy
-//     // desktop CPU
-//     session_options.SetExecutionMode(ExecutionMode::ORT_PARALLEL);
-//     session_options.SetIntraOpNumThreads(16);
-//     session_options.SetInterOpNumThreads(16);
-
-//     // General optimizations
-//     session_options.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
-
-//     struct demucsonnx::demucs_model model = load_model(
-//         model_file,
-//         session_options);
-
-//     // create 4 audio matrix same size, to hold output
-//     Eigen::Tensor3dXf audio_targets =
-//         demucsonnx::demucs_inference(model, audio, progressCallback);
-
-//     out_targets = audio_targets;
-
-//     int nb_out_sources = model.nb_sources;
-
-//     for (int target = 0; target < nb_out_sources; ++target)
-//     {
-//         // now write the 4 audio waveforms to files in the output dir
-//         // using libnyquist
-//         // join out_dir with "/target_0.wav"
-//         // using std::filesystem::path;
-
-//         std::filesystem::path p = out_dir;
-//         // make sure the directory exists
-//         std::filesystem::create_directories(p);
-
-//         auto p_target = p / "target_0.wav";
-
-//         // target 0,1,2,3 map to drums,bass,other,vocals
-
-//         std::string target_name;
-
-//         switch (target)
-//         {
-//         case 0:
-//             target_name = "drums";
-//             break;
-//         case 1:
-//             target_name = "bass";
-//             break;
-//         case 2:
-//             target_name = "other";
-//             break;
-//         case 3:
-//             target_name = "vocals";
-//             break;
-//         case 4:
-//             target_name = "guitar";
-//             break;
-//         case 5:
-//             target_name = "piano";
-//             break;
-//         default:
-//             std::cerr << "Error: target " << target << " not supported"
-//                       << std::endl;
-//             exit(1);
-//         }
-
-//         // insert target_name into the path after the digit
-//         // e.g. target_name_0_drums.wav
-//         p_target.replace_filename("target_" + std::to_string(target) + "_" +
-//                                   target_name + ".wav");
-
-//         std::cout << "Writing wav file " << p_target << std::endl;
-
-//         Eigen::MatrixXf target_waveform(2, audio.cols());
-
-//         // copy the input stereo wav file into all 4 targets
-//         for (int channel = 0; channel < 2; ++channel)
-//         {
-//             for (int sample = 0; sample < audio.cols(); ++sample)
-//             {
-//                 target_waveform(channel, sample) =
-//                     out_targets(target, channel, sample);
-//             }
-//         }
-
-//         write_audio_file(target_waveform, p_target);
-//     }
-
-//     return 0;
-// }
+  std::cout << "Processing complete. Audio data copied to output buffers."
+            << std::endl;
+}
+}
