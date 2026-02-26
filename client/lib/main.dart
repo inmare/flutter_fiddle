12f6376086b4a1b0a87ea012a0f04335973b8a4c
inmare <yatsumehole@naver.com>
2026-02-26 18:20:42 +0900

    위젯 별개의 파일로 작성

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:dart_pcm/ui/audio_player.dart';
+import 'package:dart_pcm/ui/song_search.dart';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 void main(List<String> arguments) async {
   runApp(const MaterialApp(home: App()));
@@ -13,9 +18,20 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  void _initCacheDir() async {
+    final cacheDir = await getApplicationCacheDirectory();
+    debugPrint(cacheDir.path);
+    // song 폴더가 존재하는지 확인 후 없으면 생성하기
+    final songDir = Directory(path.join(cacheDir.path, 'songs'));
+    if (!await songDir.exists()) {
+      await songDir.create(recursive: true);
+    }
+  }
+
   @override
   void initState() {
     super.initState();
+    _initCacheDir();
   }
 
   @override
@@ -25,12 +41,20 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("노래방 시뮬레이터")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [AudioPlayer()],
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const TabBar(
+            tabs: <Widget>[
+              Tab(text: "노래방", icon: Icon(Icons.play_circle_outline)),
+              Tab(text: "검색", icon: Icon(Icons.search)),
+            ],
+          ),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: const TabBarView(children: [AudioPlayer(), SongSearch()]),
         ),
       ),
     );
