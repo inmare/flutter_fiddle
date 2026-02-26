14920f845f5434b08e8443fa3dc575ff7416dfc3
inmare <yatsumehole@naver.com>
2026-02-26 21:20:48 +0900

    사용자가 추가한 곡 클래스 생성

---
+++ client/lib/audio/song_model.dart
@@ -0,0 +1,11 @@
+class DownloadedSong {
+  final int id;
+  final String title;
+  final String localPath;
+
+  DownloadedSong({
+    required this.id,
+    required this.title,
+    required this.localPath,
+  });
+}
