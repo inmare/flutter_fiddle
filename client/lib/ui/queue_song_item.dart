f7eef2c2ffd1b1a739e4d30e429cba40317062a5
inmare <yatsumehole@naver.com>
2026-02-26 21:21:43 +0900

    추가된 오디오 위젯 추가

---
+++ client/lib/ui/queue_song_item.dart
@@ -0,0 +1,27 @@
+import 'package:dart_pcm/audio/audio_manager.dart';
+import 'package:flutter/material.dart';
+
+class QueueSongItem extends StatelessWidget {
+  final int id;
+  final String name;
+  const QueueSongItem({super.key, required this.id, required this.name});
+
+  void _removeSong() {
+    AudioManager().removeSong(id);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(name),
+        ElevatedButton(
+          onPressed: () {
+            _removeSong();
+          },
+          child: Icon(Icons.delete),
+        ),
+      ],
+    );
+  }
+}
