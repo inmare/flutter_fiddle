c3d3ddc448d6ea83ab7d65037b1b99435c7647f5
inmare <yatsumehole@naver.com>
2026-02-26 21:22:45 +0900

    다운로드한 파일을 큐에 추가하는 코드 작성

--- client/lib/ui/song_item.dart
+++ client/lib/ui/song_item.dart
@@ -1,17 +1,21 @@
 import 'dart:io';
 
+import 'package:dart_pcm/audio/audio_manager.dart';
+import 'package:dart_pcm/audio/song_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as path;
 
 class SongItem extends StatelessWidget {
+  final int id;
   final String name;
   // final String artist;
   final String songLink;
   final String mrLink;
   const SongItem({
     super.key,
+    required this.id,
     required this.name,
     // required this.artist,
     required this.songLink,
@@ -19,13 +23,24 @@ class SongItem extends StatelessWidget {
   });
 
   void _downloadSong() async {
-    final response = await http.get(Uri.parse(mrLink));
-    final directory = await getApplicationCacheDirectory();
-    final filePath = path.join(directory.path, 'songs', '$name.mp3');
+    try {
+      // 파일 다운로드
+      final response = await http.get(Uri.parse(mrLink));
+      final directory = await getApplicationCacheDirectory();
+      final filePath = path.join(directory.path, 'songs', '$name.mp3');
 
-    final file = File(filePath);
-    await file.writeAsBytes(response.bodyBytes);
-    debugPrint(filePath);
+      final file = File(filePath);
+      await file.writeAsBytes(response.bodyBytes);
+      debugPrint('$filePath의 파일을 다운로드 완료했습니다');
+
+      // 파일 추가
+      final song = DownloadedSong(id: id, title: name, localPath: filePath);
+      AudioManager().addSong(song);
+      debugPrint('파일을 추가했습니다');
+      debugPrint('현재 큐에 있는 노래: ${AudioManager().queueSongs.length}개');
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   @override
